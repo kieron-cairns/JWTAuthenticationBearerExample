35c1bfab0b334948ec87ba1bc70ac919da8f80f9
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-19 17:36:50 +0100

    CHANGE: Empty controller class added

---
+++ JWTAuthenticationBearer/Controllers/JwtController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationBearer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+       
+    }
+}
